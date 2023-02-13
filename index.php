69534704939362cf2dc1682f34454824afb705a5
KEIL_Enzo <enzo.keil06@icloud.com>
2023-02-13 14:23:53 +0100

    Page index debut

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;700&family=Zen+Dots&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Challenge-48H</title>
+</head>
+
+<body>
+    <nav class="topBar">
+        <h1>Challenge-48H</h1>
+        <h3>Timer :</h3>
+        <h3>Point :</h3>
+    </nav>
+
+    <main class="main">
+        <div class="grilleTableau">
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan="10">
+                            Bataille-Navale
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                    <tr>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                        <td class="caseBataille
+                        "></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+        <div class="carousel">
+            <div class="slides">
+
+                <h1 class="slide">Morpion</h1>
+                <h1 class="slide">Juste prix</h1>
+                <h1 class="slide">Test rapidite</h1>
+                <h1 class="slide">Question cultureG</h1>
+            </div>
+            <div class="controls">
+                <div class="control prev-slide">&#9668;</div>
+                <div class="control next-slide">&#9658;</div>
+            </div>
+        </div>
+
+
+    </main>
+
+    <script src="./js/index.js"></script>
+</body>
+
+</html>
