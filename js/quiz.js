70d4b6ba7c14af20309fc3fb32a580ef05095951
Heksaeder <celine.kurtaran@gmail.com>
2023-02-14 00:07:23 +0100

    Cancelled merge + modified Quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,16 @@
 ﻿const startBtn = document.getElementById('btn-start')
 const nextBtn = document.getElementById('btn-next')
+const quitBtn = document.getElementById('btn-quit')
+const rulesElement = document.getElementById('rules')
 const qContainerElement = document.getElementById('q_container')
 const questionElement = document.getElementById('question')
+const scoreElement = document.getElementById('score')
 const repBtn = document.getElementById('answer-btns')
+const scoreDiv = document.getElementById('score')
 
-let randQuestions, curQuestionId
+let randQuestions, curQuestionId, countScore, countGame
+// countGame == nombre essai json, à décrémenter
+countGame = 0
 
 startBtn.addEventListener('click', start)
 nextBtn.addEventListener('click', () => {
@@ -12,20 +18,34 @@ nextBtn.addEventListener('click', () => {
   nextQuestion()
 })
 
+// Starts the game and sets the question randomizer 
 function start() {
   console.log('Started !')
   startBtn.classList.add('hide')
-  randQuestions = questions.sort(() => Math.random() - .5)
+  countGame+=3
+  // randQuestions = questions.sort(() => Math.random() - .5)
+  /* randQuestions = questions.sort(function(a,b) {
+    return a.niveau - b.niveau
+  }) */
+
+  randQuestions = questions.slice(countGame-3,countGame)
+
   curQuestionId = 0
+  countScore = 0
+  rulesElement.classList.add('hide')
   qContainerElement.classList.remove('hide')
+  scoreElement.classList.remove('hide')
   nextQuestion()
 }
 
+// Show next question in randomizer
 function nextQuestion() {
   resetGrid()
   showQuestion(randQuestions[curQuestionId])
 }
 
+
+// Show question and displays answers in buttons
 function showQuestion(question) {
   questionElement.innerText = question.question
   question.reponses.forEach(reponse => {
@@ -34,12 +54,17 @@ function showQuestion(question) {
     button.classList.add('btn')
     if (reponse.correct) {
       button.dataset.correct = reponse.correct
+      countScore++
+      if (countScore%3 == 1) {
+        scoreDiv.innerText = Math.floor(countScore/3) + " missile(s) acquis"
+      }
     }
     button.addEventListener('click', selectReponse)
     repBtn.appendChild(button)
   })
 }
 
+// Resets the whole answer grid
 function resetGrid() {
   clearStatusClass(document.body)
   nextBtn.classList.add('hide')
@@ -48,6 +73,7 @@ function resetGrid() {
   }
 }
 
+// Tests the answer and sets a next or quit button
 function selectReponse(e) {
   const selectBtn = e.target
   const correct = selectBtn.dataset.correct
@@ -55,15 +81,27 @@ function selectReponse(e) {
   Array.from(repBtn.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  if (randQuestions.length > curQuestionId + 1) {
+
+  // Restart loop
+  /* if (randQuestions.length > curQuestionId + 1) {
     nextBtn.classList.remove('hide')
   } else {
     startBtn.innerText = 'Restart'
     startBtn.classList.remove('hide')
+  } */
+
+  if (randQuestions.length > curQuestionId + 1) {
+    nextBtn.classList.remove('hide')
+  } else if (countGame < 9) {
+    startBtn.innerText = 'Restart'
+    startBtn.classList.remove('hide')
+  } else {
+    quitBtn.classList.remove('hide')
   }
 
 }
 
+// If correct/wrong, sets the button class to "correct" or "wrong"
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
@@ -73,32 +111,15 @@ function setStatusClass(element, correct) {
   }
 }
 
+// Clears any correct/wrong class
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
 
+
+// List of questions
 const questions = [
-  {
-    question: 'Quel est le nom actuel de la Sérénissime ?',
-    reponses: [
-      { text: 'Venise', correct: true },
-      { text: 'Rome', correct: false },
-      { text: 'Paris', correct: false },
-      { text: 'Tokyo', correct: false }
-    ],
-    niveau: 'medium'
-  },
-  {
-    question: 'Comment s\'écrit le nom de cet état américain ?',
-    reponses: [
-      { text: 'Missisipi', correct: false },
-      { text: 'Mississippi', correct: true },
-      { text: 'Misissippi', correct: false },
-      { text: 'Missisippi', correct: false }
-    ],
-    niveau: 'medium'
-  },
   {
     question: 'Quel chevalier de la Table Ronde est également appelé \"le Gallois\" ?',
     reponses: [
@@ -107,17 +128,17 @@ const questions = [
       { text: 'Lancelot', correct: false },
       { text: 'Hector', correct: false }
     ],
-    niveau: 'easy'
+    niveau: '1'
   },
   {
-    question: 'Combien de muscles y a-t-il dans le corps humain  ?',
+    question: 'Quel est le nom actuel de la Sérénissime ?',
     reponses: [
-      { text: 'Moins de 200', correct: false },
-      { text: 'Entre 200 et 400', correct: false },
-      { text: 'Entre 400 et 600', correct: false },
-      { text: 'Plus de 600', correct: true }
+      { text: 'Venise', correct: true },
+      { text: 'Rome', correct: false },
+      { text: 'Paris', correct: false },
+      { text: 'Tokyo', correct: false }
     ],
-    niveau: 'easy'
+    niveau: '2'
   },
   {
     question: 'Quelle est la signification du mot pharaon ?',
@@ -127,7 +148,27 @@ const questions = [
       { text: 'Sagesse', correct: false },
       { text: 'Qui vit sur terre', correct: false }
     ],
-    niveau: 'hard'
+    niveau: '3'
+  },
+  {
+    question: 'Combien de muscles y a-t-il dans le corps humain  ?',
+    reponses: [
+      { text: 'Moins de 200', correct: false },
+      { text: 'Entre 200 et 400', correct: false },
+      { text: 'Entre 400 et 600', correct: false },
+      { text: 'Plus de 600', correct: true }
+    ],
+    niveau: '1'
+  },
+  {
+    question: 'Comment s\'écrit le nom de cet état américain ?',
+    reponses: [
+      { text: 'Missisipi', correct: false },
+      { text: 'Mississippi', correct: true },
+      { text: 'Misissippi', correct: false },
+      { text: 'Missisippi', correct: false }
+    ],
+    niveau: '2'
   },
   {
     question: 'Que fait la cigale ?',
@@ -137,17 +178,7 @@ const questions = [
       { text: 'Elle stridule', correct: true },
       { text: 'Elle blatère', correct: false }
     ],
-    niveau: 'hard'
-  },
-  {
-    question: 'Qu\'est-ce qu\'un oléandre ?',
-    reponses: [
-      { text: 'Un grand pont', correct: false },
-      { text: 'Un laurier à fleurs', correct: true },
-      { text: 'Un arbre du Sud de la France', correct: false },
-      { text: 'Une personne généreuse', correct: false }
-    ],
-    niveau: 'hard'
+    niveau: '3'
   },
   {
     question: 'Quel nom porte le siège de la police londonienne ?',
@@ -157,7 +188,7 @@ const questions = [
       { text: 'Buckingham', correct: false },
       { text: 'Le Capitole', correct: false }
     ],
-    niveau: 'easy'
+    niveau: '1'
   },
   {
     question: 'Combien d\'États souverains bordent la mer Méditerranée ?',
@@ -167,6 +198,17 @@ const questions = [
       { text: '25', correct: false },
       { text: '31', correct: false }
     ],
-    niveau: 'medium'
-  }
+    niveau: '2'
+  },
+  {
+    question: 'Qu\'est-ce qu\'un oléandre ?',
+    reponses: [
+      { text: 'Un grand pont', correct: false },
+      { text: 'Un laurier à fleurs', correct: true },
+      { text: 'Un arbre du Sud de la France', correct: false },
+      { text: 'Une personne généreuse', correct: false }
+    ],
+    niveau: '3'
+  } 
+  
 ]
--- quiz/quiz.php
+++ quiz/quiz.php
@@ -4,16 +4,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/quiz.css">
-  <script defer src="../js/quiz.js"></script>
+  <link rel="stylesheet" href="quiz_style.css">
+  <script defer src="quiz.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet">
-  <title>QUIZ</title>
+  <title>Challenge 48H</title>
 </head>
 
 <body>
   <div class="container">
+    <div id="rules"><h1>C'est l'heure du quiz !</h1>
+    <p>Ce mini-jeu consiste en un simple quiz de culture générale. Sport, cinéma, politique, histoire et autres domaines seront à l'honneur dans une suite aléatoire de devinettes.</p>
+    <p>I. Une série de trois questions vous sera présentée, dans l'ordre croissant de difficulté. <br>
+    II. Cliquer sur une réponse la valide automatiquement, gardez vos doigts en laisse. <br>
+    III. Afin de remporter un missile, vous devrez répondre correctement aux <strong>trois</strong> questions. <br>
+    IV. Bien sûr, se servir d'Internet n'est pas autorisé, pas plus qu'inspecter les scripts !<br>
+    V. Vous aurez droit à trois parties <strong>maximum</strong>, tâchez de les jouer intelligemment.<br><br>
+    Attention : vous ne pourrez pas revenir en arrière. </p></div>
+
     <div id="q_container" class="hide">
       <div id="question">Question</div>
       <div id="answer-btns" class="btn-grid">
@@ -27,7 +36,10 @@
     <div class="controls">
       <button id="btn-start" class="btn-start btn">Start</button>
       <button id="btn-next" class="btn-next btn hide">Next</button>
+      <button id="btn-quit" class="btn-quit btn hide" onclick="location='../index.php'">Quit</button>
     </div>
+
+    <div id="score" class="hide"></div>
   </div>
 </body>
 </html>
