4556b46e81b4e0ddb2500aededf6aafe19e4e76f
MikeXXI <mike.dje@outlook.fr>
2023-02-14 01:44:46 +0100

    Update morpion

--- js/morpion.js
+++ js/morpion.js
@@ -1,61 +1,80 @@
-var cases = document.getElementsByClassName("board");
+// Fonction pour mettre à jour l'affichage du jeu
+function render() {
+    for (var i = 0; i < board.length; i++) {
+        document.getElementById(i).innerText = board[i];
+    }
+    statusEl.innerText = "C'est à vous de jouer !";
+}
 
-var board = ["", "", "", "", "", "", "", "", ""]; // tableau pour stocker l'état du jeu
-var player = "O"; // joueur 
-var ordi = "X"; // ordi
-var statusEl = document.getElementById("status");
-//statusEl.innerText = "C'est à vous de jouer!"; // élément pour afficher le statut du jeu
-var resetBtn = document.getElementById("reset"); // bouton pour rejouer
+// Fonction pour initialiser le jeu
+function init() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    player = "O";
+    ordi = "X";
+    statusEl = document.getElementById("status");
+    statusEl.innerText = "C'est à vous de jouer !";
+    render();
+}
 
+// Fonction pour démarrer une partie
+function start() {
+    init();
+    for (i = 0; i < 9; i++) {
+        addEventBtn = document.getElementById(i)
+        addEventBtn.onclick = play;
+    }
+    changeBtn = document.getElementById("start");
+    changeBtn.type = 'hidden';
+    changeBtn = document.getElementById("reset");
+    changeBtn.type = 'button';
+    statusEl.innerText = "C'est à vous de jouer !";
 
+}
+
+// Fonction pour relancer une partie
+function reset() {
+    console.log("reset");
+    init();
+}
+
+// Fonction pour jouer
 function play() {
     let fond = document.getElementById('board');
     fond.addEventListener('click', (e) => {
         let elementId = e.target.id;
         let element = document.getElementById(elementId);
         if (element != null && element.classList.contains('square')) {
-            board[elementId] = player;
-            render();
-            if (isGameOver()) {
-                if (isWinner(player)) {
-                    statusEl.innerText = "Vous avez gagné !";
-                }
-                return;
-            }
-            if (isBoardFull()) {
-                statusEl.innerText = "Match nul !";
-                return;
+            if (!element.classList.contains('clicked')) {
+                check(elementId, player);
+                do {
+                    tourOrdi = random(0, 8);
+                    console.log(tourOrdi);
+                } while (board[tourOrdi] != "");
+                check(tourOrdi, ordi);
             }
         }
-        tourOrdi = random(0, 8);
-        console.log(tourOrdi);
-        if (board[tourOrdi] == ""){
-            joueur = "Player"
-            board[tourOrdi] = ordi;
-            render();
-            if (isGameOver()) {
-                if (isWinner(ordi)) {
-                    statusEl.innerText = "Vous avez perdu !";
-                }else{
-                    statusEl.innerText = "Match nul !";
-                }
-                return;
-            }
-        }
-       
-
     });
-    
-
 }
 
-
-// Fonction pour mettre à jour l'affichage du jeu
-function render() {
-    for (var i = 0; i < board.length; i++) {
-        document.getElementById(i).innerText = board[i];
+// Fonction de vérification
+function check(indice, result) {
+    if (board[indice] == "") {
+        board[indice] = result;
+        document.getElementById(indice).classList.add('clicked')
+        document.getElementById(indice).innerText = board[indice];
+        if (isGameOver()) {
+            if (isWinner(result)) {
+                statusEl.innerText = "Vous avez gagné !";
+            }
+            return;
+        }
+        if (isBoardFull()) {
+            statusEl.innerText = "Match nul !";            
+        }
+        return;
+    } else {
+        console.log("case déjà prise");
     }
-    statusEl.innerText = "Tour de " + player;
 }
 
 // Fonction pour vérifier si le jeu est terminé
@@ -91,9 +110,15 @@ function isWinner(player) {
             return true;
         }
     }
+    // vérifier les diagonales
+    if (board[0] == player && board[4] == player && board[8] == player) {
+        return true;
+    }
+    if (board[2] == player && board[4] == player && board[6] == player) {
+        return true;
+    }
 }
 
-function random(min, max)
-{
- return Math.floor(Math.random() * (max - min + 1)) + min;
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
