bfa9e99c85ab1cb7a17b6dfc54af66e0184e18f4
MikeXXI <mike.dje@outlook.fr>
2023-02-14 12:55:20 +0100

    update morpion.php

--- morpion/morpion.php
+++ morpion/morpion.php
@@ -7,9 +7,28 @@ var max_tentative = <?php echo $data["jeux"][0]['max_tentative'] ?>;
 </script>
 
 <!DOCTYPE html>
-<html lang="fr">
-<?php include ("header.php"); ?>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/justeprix.css">
+    <link rel="stylesheet" href="../css/morpion.css">
+    <link rel="stylesheet" href="../css/textWriter.css">
+    <link rel="stylesheet" href="../css/timer.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
+    <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
+    <title>Challenge-48H</title>
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+</head>
+
+
+<nav class="topBar">
+    <h1><a href="../index.php">Challenge-48H</a></h1>
+    <h3>Timer : <div id="timer">
+    </h3>
+</nav>
 <body>
     <div class="overlay"></div>
     <div id="modal" class="modal">
@@ -45,7 +64,7 @@ var max_tentative = <?php echo $data["jeux"][0]['max_tentative'] ?>;
             </div>
         </div>
         <div class="containerCarousel">
-            <img src="../img/title_morpion.png" alt="morpion" id="morpion" />
+            <img src="<?php $route?>img/title_morpion.png" alt="morpion" id="morpion" />
             <h1 class="titleMissile">
                 <span class="span">
                     <?php echo $data['missile']; ?>
