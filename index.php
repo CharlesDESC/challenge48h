dcaa07e582d1699f2df8d37bf7f7832d1ce8269b
KEIL_Enzo <enzo.keil06@icloud.com>
2023-02-14 14:03:30 +0100

    quiz liaison

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ $data = json_decode($json, true);
     <link rel="stylesheet" href="./css/morpion.css">
     <link rel="stylesheet" href="./css/textWriter.css">
     <link rel="stylesheet" href="./css/timer.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
     <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
     <title>Challenge-48H</title>
     <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
@@ -197,8 +198,9 @@ $data = json_decode($json, true);
                 <div class="slides">
                     <a href="./morpion/morpion.php"><img src="./img/title_morpion_orange.png" alt="Morpion"></a>
                     <a href="./justeprix/justeprix.php"><img src="./img/justePrix.png" alt="justePrix"></a>
-                    <a href="#"><img src="./img/cultureG.png" alt="cultureG"></a>
-                    <a href="./textWriter/textWriter.php"><img src="./img/rapiditeClavier.png" alt="rapiditeClavier"></a>
+                    <a href="./quiz/quiz.php"><img src="./img/cultureG.png" alt="cultureG"></a>
+                    <a href="./textWriter/textWriter.php"><img src="./img/rapiditeClavier.png"
+                            alt="rapiditeClavier"></a>
                     <a href="./mastermind/mastermind.php"><img src="./img/mastermind.png" alt="mastermind"></a>
                 </div>
                 <div class="controls">
