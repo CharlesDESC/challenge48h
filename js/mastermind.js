83684af24ccb4e9c68a2fc9ffdfd01a5f8320313
lmateo <mateo.luque@ynov.com>
2023-02-14 10:22:14 +0100

    mind work

--- js/mastermind.js
+++ js/mastermind.js
@@ -2,68 +2,84 @@ let varCopie = [];
 let varEssai = [];
 let nbEssai = 0;
 let codeplace = 0;
+let BP = 0;
+let MP = 0;
 
 let essai = document.getElementById('essai');
 
+let place = document.getElementById('place');
+
 let rougebut = document.getElementById('rouge');
-rougebut.addEventListener('click', main('rouge'));
+rougebut.onclick = function () { Main('rouge') };
 
 let bleubut = document.getElementById('bleu');
-bleubut.addEventListener('click', main('bleu'));
+bleubut.onclick = function () { Main('bleu') };
 
 let vertbut = document.getElementById('vert');
-vertbut.addEventListener('click', main('vert'));
+vertbut.onclick = function () { Main('vert') };
 
 let jaunebut = document.getElementById('jaune');
-jaunebut.addEventListener('click', main('jaune'));
+jaunebut.onclick = function () { Main('jaune') };
 
 let rosebut = document.getElementById('rose');
-rosebut.addEventListener('click', main('rose'));
+rosebut.onclick = function () { Main('rose') };
 
 let orangebut = document.getElementById('orange');
-orangebut.addEventListener('click', main('orange'));
+orangebut.onclick = function () { Main('orange') };
 
 let blancbut = document.getElementById('blanc');
-blancbut.addEventListener('click', main('blanc'));
+blancbut.onclick = function () { Main('blanc') };
 
 let varCoul = ['rouge', 'bleu', 'vert', 'jaune', 'rose', 'orange', 'blanc'];
 
 let varOriginal = [];
 for (let step = 0; step < 4; step++) {
-    varOriginal = varOriginal.concat(random_item(varCoul));
+    varOriginal = varOriginal.concat(Random_item(varCoul));
 }
 
-function addColor(color, place) {
+
+essai.innerHTML += "Essais \xa0 : <br>"
+essai.innerHTML += "Essai " + (nbEssai + 1) + " \xa0 : \xa0 ";
+place.innerHTML += "Bien placé : \xa0\xa0\xa0 Mal placé :"
+
+function AddColor(color, place) {
     varEssai[place] = color;
-    varCopie[place] = color;
+    varCopie[place] = varOriginal[place];
     essai.innerHTML += color + "    ";
 }
 
-function main(color) {
-    console.log(color)
-    if (codeplace > 3) {
-        let BP = calculBP(varCopie, varEssai);
-        let MP = calculMP(varCopie, varEssai);
-        if (BP < 4 && nbEssai < 20) {
-            essai.innerHTML += "<br>Essai " + nbEssai + " :";
-        } else {
-            if (BP >= 4) {
-                alert("you win");
+function Main(color) {
+    if (nbEssai < 20 && BP < 4) {
+        console.log(color)
+        AddColor(color, codeplace);
+        codeplace++;
+        if (codeplace > 3) {
+            BP = CalculBP(varCopie, varEssai);
+            MP = CalculMP(varCopie, varEssai);
+            place.innerHTML += "<br>" + BP + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + MP;
+            if (BP < 4 && nbEssai < 20) {
+                nbEssai++;
+                if (nbEssai < 20) {
+                    essai.innerHTML += "<br>Essai " + (nbEssai + 1) + " \xa0 : \xa0 ";
+                }
             } else {
-                alert("you loose");
+                if (BP >= 4) {
+                    alert("you win");
+                } else {
+                    alert("you loose");
+                }
+
             }
+            codeplace = 0;
         }
-        codeplace = 0;
     }
-    addColor(color, codeplace);
-    codeplace++;
 }
 
-function random_item(items) {
+function Random_item(items) {
     return items[Math.floor(Math.random() * items.length)];
 }
 
-function calculBP(vec1, vec2) {
+function CalculBP(vec1, vec2) {
     let bp = 0;
     for (let i = 0; i < 4; i++) {
         if (vec1[i] == vec2[i]) {
@@ -75,12 +91,12 @@ function calculBP(vec1, vec2) {
     return bp;
 }
 
-function calculMP(vec1, vec2) {
+function CalculMP(vec1, vec2) {
     let mp = 0;
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             if (vec1[i] == vec2[j]) {
-                bp++;
+                mp++;
                 vec1[i] = 'X';
                 vec2[j] = 'Y';
             }
@@ -88,18 +104,6 @@ function calculMP(vec1, vec2) {
     }
     return mp;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
